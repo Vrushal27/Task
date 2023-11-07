4fd1260397d34153f808a1989d407308e5279c5a
Vrushal27 <vrushal78693@gmail.com>
2023-11-07 16:59:21 +0530

    update

---
+++ index.js
@@ -0,0 +1,32 @@
+
+var students = []; 
+
+function addStudent(rowNum, name, location, cgpa) {
+  var student = { rowNum: rowNum, name: name, location: location, cgpa: cgpa };
+  students.push(student);
+}
+
+function removeStudent(rowNum) {
+  for (var i = 0; i < students.length; i++) {
+    if (students[i].rowNum === rowNum) {
+      students.splice(i, 1);
+      return;
+    }
+  }
+}
+
+function addRow() {
+  var rowNum = students.length + 1;
+  var name = ""; 
+  var location = ""; 
+  var cgpa = ""; 
+  addStudent(rowNum, name, location, cgpa);
+
+  
+}
+
+function deleteRow(rowNum) {
+  removeStudent(rowNum);
+
+
+}
